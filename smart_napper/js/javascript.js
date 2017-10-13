dd22944ea0848e81cbfc4bd3417e99bb9b2da1e8
Samiullah Jamil <jamil.samiullah@yahoo.com>
2017-10-13 09:49:11 +0500

    Change of Name

---
+++ smart_napper/js/javascript.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {// get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+    var mainbottom = /*$('.about').offset().top + */$('.navbar').height();
+// on scroll, 
+    $(window).scroll(function(){
+    // we round here to reduce a little workload
+        var stop = Math.round($(window).scrollTop());
+        if (stop > mainbottom) {
+            $('.navbar').addClass('navbar-change');
+        } else {
+            $('.navbar').removeClass('navbar-change');
+        }
+    });
+    $('a.scrollbtn').click(function(e) {
+        //e.preventDefault();
+        var el = $(this).attr("href");
+        var top = $(el).offset().top;
+        $('body').animate({scrollTop:top-mainbottom},1000);
+    });
+});
